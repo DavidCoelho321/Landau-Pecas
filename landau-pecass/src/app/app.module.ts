dbfc4cbe2982e0aacd5212e4f946107503a4259d
Ivan-Souza92 <ivan.souza92@hotmail.com>
2021-10-21 11:24:04 -0300

    commit component

--- landau-pecass/src/app/app.module.ts
+++ landau-pecass/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './template/topo/topo.component';
+import { HomeComponent } from './template/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,TopoComponent,HomeComponent]
 })
 export class AppModule { }
