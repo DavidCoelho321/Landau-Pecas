7cd14903f7143ef99f123a5c6197ec446682ab93
DavidCoelho321 <david123coelho@gmail.com>
2021-11-03 19:35:05 -0300

    atualização de cadastro

--- landau-pecass/src/app/app.module.ts
+++ landau-pecass/src/app/app.module.ts
@@ -3,28 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { CadastroComponent } from './cadastro/cadastro.component';
-=======
+
 import { TopoComponent } from './template/topo/topo.component';
 import { HomeComponent } from './template/home/home.component';
->>>>>>> c7b79eb88a7bfc1ccdf4a20cc4d1bf1a9ad9f323
+import { LoguinComponent } from './loguin/loguin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    CadastroComponent
-=======
+    CadastroComponent,
     TopoComponent,
-    HomeComponent
->>>>>>> c7b79eb88a7bfc1ccdf4a20cc4d1bf1a9ad9f323
+    HomeComponent,
+    LoguinComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent,TopoComponent,HomeComponent]
+  bootstrap: [AppComponent,TopoComponent]
 })
 export class AppModule { }
