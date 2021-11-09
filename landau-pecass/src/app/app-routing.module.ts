45e7e5f800ab33eb5b65bc0bc5e5b30d5a84d217
Ivan-Souza92 <ivan.souza92@hotmail.com>
2021-11-09 08:19:52 -0300

    atualizando a rota

--- landau-pecass/src/app/app-routing.module.ts
+++ landau-pecass/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 import { HomeComponent } from './template/home/home.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'login',component:LoginComponent},
-  {path:'cadastro',component:CadastroComponent}
+  {path:'cadastro',component:CadastroComponent},
+  {path:'produtos', component:ProdutosComponent}
 ];
 
 @NgModule({
